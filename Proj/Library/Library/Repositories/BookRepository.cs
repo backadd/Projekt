30597fad6b2f4dd526128112cf5ad1a080b08c3e
Backadd <backadded@gmail.com>
2017-10-19 21:02:41 +0200

    km,hgjk

--- Proj/Library/Library/Repositories/BookRepository.cs
+++ Proj/Library/Library/Repositories/BookRepository.cs
@@ -12,8 +12,36 @@ namespace Library.Repositories {
             _context = ctx;
         }
 
+        public void Add(Book item)
+        {
+            _context.Books.Add(item);
+        }
+
         public IEnumerable<Book> All() {
             return _context.Books.ToList();
+        }
+
+        public void Edit(Book item)
+        {
+            Book BookToDelete = Find(item.Id);
+            Remove(BookToDelete);
+            Add(item);
+        }
+
+        public Book Find(int id)
+        {
+            var books = _context.Books.Where(b => b.Id == id).ToList();
+            if (books.Count > 0)
+            {
+                return books[0];
+            }
+            return null;
+
+        }
+
+        public void Remove(Book item)
+        {
+            _context.Books.Remove(item);
         }
     }
 }
