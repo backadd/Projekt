f1d1f4f1e9521ba514fb596f324f4cc485c85926
Backadd <backadded@gmail.com>
2017-10-19 20:33:56 +0200

    Remove ignored files

--- Proj/Library/Library/Models/Book.cs
+++ Proj/Library/Library/Models/Book.cs
@@ -7,12 +7,27 @@ using System.Web;
 namespace Library.Models {
     public class Book {
 
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [ForeignKey("AuthorRef")]
+        public virtual Author Author { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public ICollection<BookCopy> Copies { get; set; }
 
-        /// <summary>
-        /// Useful for adding the book objects directly to a ListBox.
-        /// </summary>
+        public Book(int id, string title, Author author, string isbn, string description)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            Description = description;
+            Copies = new List<BookCopy>();
+        }
         public override string ToString() {
             return String.Format("[{0}] -- {1}", this.Id, this.Title);
         }
